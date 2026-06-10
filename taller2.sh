394f54b4da2246c1aa10c8dae6f4e8f64da6cf43
Lucas Ahumada <127716294+lucasahu@users.noreply.github.com>
2026-06-10 13:22:16 -0400

    agregar esqueleto al .sh

--- taller2.sh
+++ taller2.sh
@@ -1,1 +1,32 @@
-
+#!/bin/bash
+ 
+PIDS=()
+SERVICIOS=()
+ 
+listar_procesos() {
+    : # TODO Issue #2
+}
+ 
+matar_proceso() {
+    : # TODO Issue #3
+}
+ 
+listar_servicios() {
+    : # TODO Issue #4
+}
+ 
+activar_servicio() {
+    : # TODO Issue #5
+}
+ 
+while true; do
+    read -p "Opción [1-5]: " opcion
+    case "$opcion" in
+        1) listar_procesos ;;
+        2) matar_proceso ;;
+        3) listar_servicios ;;
+        4) activar_servicio ;;
+        5) break ;;
+        *) ;;
+    esac
+done
